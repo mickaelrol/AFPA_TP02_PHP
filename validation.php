66b067bf4ce55afc8057516e3f49d63520baa2c7
mickael <mickaelrol.devwwm79@gmail.com>
2020-10-07 14:19:51 +0200

    Update validation.php

--- validation.php
+++ validation.php
@@ -3,7 +3,7 @@
 require 'config.php';
 
 session_start();
-$_SESSION['erreur'] ="";
+$_SESSION['erreur'];
 
 $_SESSION['CONNECT'];
 
@@ -26,19 +26,15 @@ if ($login == "" || $mdp == "") {
     // log ou mdp vide , redirection vers page location.php avec message erreur vide
     $_SESSION['erreur'] = 1;
     header ('Location:login.php');
-    
     exit();
 }elseif($login !== LEBONLOGIN ||$mdp !== LEBONPASS ){
     $_SESSION['erreur'] = 2;
     header ('location:login.php');
-    
     exit();
 }else{
     // login et mdp correct redirection vers page acceuil.php .
     $_SESSION['CONNECT'] = 'OK';
     header('location:acceuil.php');
-    
     exit();
-
 }
 
