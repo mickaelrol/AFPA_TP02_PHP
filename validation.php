b3dab5a18f52d89c39ef2500f9755d328729e461
mickael <mickaelrol.devwwm79@gmail.com>
2020-10-07 12:24:35 +0200

    Update validation.php

--- validation.php
+++ validation.php
@@ -3,29 +3,42 @@
 require 'config.php';
 
 session_start();
-$_SESSION['errorcode'];
+$_SESSION['erreur'];
+
+$_SESSION['CONNECT'];
 
 // ont cree 2 variables pour recuperer le login et mdp du formulaire .
 $login = (htmlspecialchars($_POST['login']));
 $mdp = (htmlspecialchars($_POST['mdp']));
 
+$_SESSION['login'] = $_POST['login'];
+$_SESSION['mdp'] = $_POST['mdp'];
 
-
+$affaire = $_GET['affaire'];
+if ($affaire == 'deconnexion') {
+    session_unset();
+    session_destroy();
+    $_SESSION['erreur'] = 3;
+}
 
 //ont cree une condition if qui va renvoyer vers 2 page d'erreurs soit si le champ est vide ou si il ne correspond pas au bon login et mdp .
 if ($login == "" || $mdp == "") {
     // log ou mdp vide , redirection vers page location.php avec message erreur vide
+    $_SESSION['erreur'] = 1;
     header ('Location:login.php');
-    $_SESSION['errorcode'] = 1;
+    
     exit();
-}else if($login !== LEBONLOGIN ||$mdp !== LEBONPASS ){
+}elseif($login !== LEBONLOGIN ||$mdp !== LEBONPASS ){
+    $_SESSION['erreur'] = 2;
     header ('location:login.php');
-    $_SESSION['errorcode'] = 2;
+    
     exit();
 }else{
     // login et mdp correct redirection vers page acceuil.php .
+    $_SESSION['CONNECT'] = 'OK';
     header('location:acceuil.php');
     
     exit();
 
 }
+
