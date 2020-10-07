384353350e881424b45e27c5d7a4e7b7c55c89d6
mickael <mickaelrol.devwwm79@gmail.com>
2020-10-07 12:24:48 +0200

    Update acceuil.php

--- acceuil.php
+++ acceuil.php
@@ -1,3 +1,10 @@
 <?php
 session_start();
-echo 'Hello';
+if($_SESSION['CONNECT'] !== 'OK'){
+    $_SESSION['erreur']="";
+    header ('location:login.php');
+    exit();
+}else{
+    echo 'Hello ' .$_SESSION['login'];
+    echo ('<a href="validation.php?affaire=deconnexion">Deconnexion</a>');
+}
