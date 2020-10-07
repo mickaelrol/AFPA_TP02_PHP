0fb7eb4959fa26c6473478a309f223622c1f43e6
mickael <mickaelrol.devwwm79@gmail.com>
2020-10-07 11:23:24 +0200

    Update validation.php

--- validation.php
+++ validation.php
@@ -2,27 +2,30 @@
 // ont recupere les données de config.php .
 require 'config.php';
 
+session_start();
+$_SESSION['errorcode'];
 
 // ont cree 2 variables pour recuperer le login et mdp du formulaire .
 $login = (htmlspecialchars($_POST['login']));
 $mdp = (htmlspecialchars($_POST['mdp']));
-$vide = 'Veuillez saisir un login et un mot de passe';
-$wrong = 'Erreur de login / Mot de passe';
+
+
 
 
 //ont cree une condition if qui va renvoyer vers 2 page d'erreurs soit si le champ est vide ou si il ne correspond pas au bon login et mdp .
 if ($login == "" || $mdp == "") {
     // log ou mdp vide , redirection vers page location.php avec message erreur vide
     header ('Location:login.php');
-    echo $vide;
+    $_SESSION['errorcode'] = 1;
     exit();
 }else if($login !== LEBONLOGIN ||$mdp !== LEBONPASS ){
     header ('location:login.php');
-    echo $wrong;
+    $_SESSION['errorcode'] = 2;
     exit();
 }else{
     // login et mdp correct redirection vers page acceuil.php .
     header('location:acceuil.php');
-    exit();
     
+    exit();
+
 }
